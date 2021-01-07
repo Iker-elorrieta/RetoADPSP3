c8bcd20fcba41cb82cb0d6549f24a1d02bd794cd
in2dam-b <in2dam-b@HZ061510.elorrieta.int>
2021-01-07 17:05:24 +0100

    primera posible solucion hash

--- src/hilos/HiloActualizarBaseDeDatos.java
+++ src/hilos/HiloActualizarBaseDeDatos.java
@@ -3,13 +3,15 @@ package hilos;
 import java.util.ArrayList;
 
 public class HiloActualizarBaseDeDatos extends Thread{
+
 private ArrayList<Objetos.Links> links = new ArrayList<Objetos.Links>();
 private Hash.Diarios diario = new Hash.Diarios();
 private Hash.Estaciones estaciones = new Hash.Estaciones();
 private Hash.Playas playas = new Hash.Playas();
 private Hash.Pueblos pueblos = new Hash.Pueblos();
 private Hash.Links link = new Hash.Links();
-	public void Run(){
+	
+public void run(){
 		
 		diario.comprobarHashDiarios(links);
 		estaciones.comprobarHashEstaciones();
