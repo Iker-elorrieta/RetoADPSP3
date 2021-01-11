a14afb9d3a99f92d291e15cfc446352eb8745c9b
ibai <ibai@DESKTOP-SJOK5TP>
2021-01-11 22:40:34 +0100

    se la subo a el

--- Json/leerJson/leerJson.java
+++ Json/leerJson/leerJson.java
@@ -278,6 +278,7 @@ public class leerJson {
 							 espaciosNaturales=new EspaciosNaturales();
 							 estaEn = new EstaEn();
 							 estaEnId=new EstaEnId();
+							 estaVacio=false;
 							 
 						}
 						
