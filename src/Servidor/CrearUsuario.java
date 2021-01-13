03b03862550e652f84d39a41b1eb5266e335f689
in2dam-b <in2dam-b@HZ061516.elorrieta.int>
2021-01-13 18:43:45 +0100

    comit servidor actualizado

---
+++ src/Servidor/CrearUsuario.java
@@ -0,0 +1,69 @@
+package Servidor;
+
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import Insert.HibernateUtil;
+import Tablas.Provincias;
+import Tablas.Usuarios;
+
+public class CrearUsuario {
+	String usuario;
+	String pass;
+	String dat ="";
+	public SessionFactory sesion= HibernateUtil.getSessionFactory();
+	public Session session= sesion.openSession();
+	public CrearUsuario(String datos) {
+		dat = datos;
+		
+	}
+	
+	public int separardatos(){
+		try {
+			String inf[] = dat.split(":");
+			usuario = inf[0];
+			pass = inf[1];
+			return 1;
+		}catch(Exception e) {
+			return -1;
+		}
+		
+	}
+	
+	public String ConsultarDatos() {
+		boolean agregar = true;
+		String hql= "From Usuarios";
+		Query q= (Query) session.createQuery(hql);
+		List <Usuarios> dep= q.list();
+		
+		for(int i=0;i<dep.size();i++) {
+			Usuarios d = dep.get(i);
+			if(d.getNombre().equals(usuario)) {
+				agregar = false;
+				
+			}
+			
+		}
+		
+		if(agregar) {
+			/*SessionFactory sesion = HibernateUtil.getSessionFactory();
+			Session session = sesion.openSession();*/
+			session.beginTransaction();
+			Usuarios usu = new Usuarios();
+			usu.setNombre(usuario);
+			usu.setPass(pass);
+			usu.setCodUsuario(dep.size());
+			session.save(usu);
+			session.getTransaction().commit();
+			System.out.println("Usuario Creado");
+			session.close();
+			return "ok";
+		}else {
+			return "error";
+			}
+	}
+
+}
--- src/Servidor/Servidor.java
+++ src/Servidor/Servidor.java
@@ -8,8 +8,10 @@ import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+
 public class Servidor {
 	private final static int PUERTO = 5000;
+	static boolean activo = true;
 	
 	public static void main(String[] args) {
 		iniciar();
@@ -20,16 +22,24 @@ public static void iniciar() {
 		Socket cliente = null;
 		DataInputStream entrada = null;
 		DataOutputStream salida = null;
+		
+		
 		try {
 			servidor = new ServerSocket(PUERTO);
-			System.out.println("Esperando conexiones del cliente...");
-			cliente = servidor.accept();
-			System.out.println("Cliente conectado");
-			salida = new DataOutputStream (cliente.getOutputStream());
-			entrada = new DataInputStream(cliente.getInputStream());
-			
-			salida.writeUTF("Bienvenido al servidor del equipo 3");
-			
+			while (activo) {
+				
+				System.out.println("Esperando conexiones del cliente...");
+				cliente = servidor.accept();
+				System.out.println("Cliente conectado");
+				salida = new DataOutputStream (cliente.getOutputStream());
+				entrada = new DataInputStream(cliente.getInputStream());
+				
+				Hiloconecxiones hr = new Hiloconecxiones(entrada, salida);
+				Thread h1 = new Thread(hr);
+				h1.start();
+				
+				
+			}
 		} catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
 		} catch (Exception e) {
